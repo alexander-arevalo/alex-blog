1c6f8432a84dae4bec95268a2b8afdf6b8fbbcbc
alexander-arevalo <zanderarevalo20@gmail.com>
2024-09-06 19:15:48 +0800

    added zod

--- app/action.ts
+++ app/action.ts
@@ -1,11 +1,33 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
+import { parseWithZod } from "@conform-to/zod";
+import { siteSchema } from "./utils/zod-schemas";
+import prisma from "./utils/db";
 
-export async function CreateSiteAction() {
+export async function CreateSiteAction(formData: FormData) {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
 
   if (!user) {
     return redirect("api/auth/login");
   }
+
+  const submission = parseWithZod(formData, {
+    schema: siteSchema,
+  });
+
+  if (submission.status !== "success") {
+    return submission.reply();
+  }
+
+  const response = await prisma.site.create({
+    data: {
+      description: submission.value.description,
+      name: submission.value.name,
+      subdirectory: submission.value.subdirectory,
+      userId: user.id,
+    },
+  });
+
+  return redirect("dashboard/sites");
 }
--- app/dashboard/sites/new/page.tsx
+++ app/dashboard/sites/new/page.tsx
@@ -21,25 +21,29 @@ const page = () => {
             Create your Site here. Click the button below once your done...
           </CardDescription>
         </CardHeader>
-        <CardContent>
-          <div className="flex flex-col gap-y-6">
-            <div className="grid gap-3">
-              <Label>Site name</Label>
-              <Input placeholder="Site name" />
+        <form>
+          <CardContent>
+            <div className="flex flex-col gap-y-6">
+              <div className="grid gap-3">
+                <Label>Site name</Label>
+                <Input placeholder="Site name" />
+              </div>
+
+              <div className="grid gap-3">
+                <Label>Subdirectory</Label>
+                <Input placeholder="Subdirectory" />
+              </div>
+
+              <div className="grid gap-3">
+                <Label>Description</Label>
+                <Textarea placeholder="Small description for your site" />
+              </div>
             </div>
-            <div className="grid gap-3">
-              <Label>Subdirectory</Label>
-              <Input placeholder="Subdirectory" />
-            </div>
-            <div className="grid gap-3">
-              <Label>Description</Label>
-              <Textarea placeholder="Small description for your site" />
-            </div>
-          </div>
-        </CardContent>
-        <CardFooter>
-          <Button>Submit</Button>
-        </CardFooter>
+          </CardContent>
+          <CardFooter>
+            <Button>Submit</Button>
+          </CardFooter>
+        </form>
       </Card>
     </div>
   );
